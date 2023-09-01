b9235514a37d9a0ccb4bf30a907e0c094a1d843f
Zach Karr <zachmkarr@gmail.com>
2023-09-01 03:51:32 -0400

    updated schema

---
+++ tables.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE "User" (
+    "id" serial PRIMARY KEY,
+    "email" varchar(100) UNIQUE NOT NULL,
+    "username" varchar(100) UNIQUE NOT NULL,
+    "password" varchar(100) NOT NULL
+);
+
+CREATE TABLE "Recipe" (
+    "id" serial PRIMARY KEY,
+    "name" varchar(100) NOT NULL,
+    "ingredients" varchar(200) NOT NULL,
+    "instructions" text NOT NULL,
+    "image" varchar(200) NOT NULL
+);
+
+CREATE TABLE "saved_recipes" (
+    "user_id" integer REFERENCES "User" ("id"),
+    "recipe_id" integer REFERENCES "Recipe" ("id"),
+    PRIMARY KEY ("user_id", "recipe_id")
+);
+
